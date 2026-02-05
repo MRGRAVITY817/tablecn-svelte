e88f0b8640f3fd44b3d7f8c2cc3a5e0956e73364
Hoon Wee <mrgravity817@gmail.com>
2026-02-05 21:07:09 +0900

    Phase 1.1-1.4: Setup Tailwind, dependencies, and base utilities

---
+++ src/lib/types/data-table.ts
@@ -0,0 +1,59 @@
+import type { ColumnSort, Row, RowData } from '@tanstack/svelte-table';
+import type { DataTableConfig } from '@/config/data-table';
+import type { ComponentType } from 'svelte';
+
+declare module '@tanstack/svelte-table' {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	interface TableMeta<TData extends RowData> {
+		queryKeys?: QueryKeys;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	interface ColumnMeta<TData extends RowData, TValue> {
+		label?: string;
+		placeholder?: string;
+		variant?: FilterVariant;
+		options?: Option[];
+		range?: [number, number];
+		unit?: string;
+		icon?: ComponentType;
+	}
+}
+
+export interface QueryKeys {
+	page: string;
+	perPage: string;
+	sort: string;
+	filters: string;
+	joinOperator: string;
+}
+
+export interface Option {
+	label: string;
+	value: string;
+	count?: number;
+	icon?: ComponentType;
+}
+
+export type FilterOperator = (typeof DataTableConfig)['operators'][number];
+export type FilterVariant = (typeof DataTableConfig)['filterVariants'][number];
+export type JoinOperator = (typeof DataTableConfig)['joinOperators'][number];
+
+export interface ExtendedColumnSort<TData> extends Omit<ColumnSort, 'id'> {
+	id: Extract<keyof TData, string>;
+}
+
+export interface FilterItem {
+	id: string;
+	operator: FilterOperator;
+	value: string | number | Date | string[] | [number, number] | null;
+}
+
+export interface ExtendedColumnFilter<TData> extends FilterItem {
+	id: Extract<keyof TData, string>;
+}
+
+export interface DataTableRowAction<TData> {
+	row: Row<TData>;
+	variant: 'update' | 'delete';
+}
